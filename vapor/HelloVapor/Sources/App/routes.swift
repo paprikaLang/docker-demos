0738b98779facfc97a3616e4093492c0f7626adc
paprikaLang <3060866223@qq.com>
2018-08-27 21:41:39 +0800

    fix(HelloVapor .git)

---
+++ vapor/HelloVapor/Sources/App/routes.swift
@@ -0,0 +1,16 @@
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    // Basic "Hello, world!" example
+    router.get("hello",String.parameter) { req->String in
+        let name = try! req.parameters.next(String.self)
+        return "Hello, \(name)!"
+    }
+
+    // Example of configuring a controller
+    let todoController = TodoController()
+    router.get("todos", use: todoController.index)
+    router.post("todos", use: todoController.create)
+    router.delete("todos", Todo.parameter, use: todoController.delete)
+}
